dd4b6c3d6a5be205c039392412dc704edcf861ee
sohol1324 <50266496+sohol1324@users.noreply.github.com>
2022-04-19 14:51:53 +0900

    Add files via upload
    화면 사이즈 조절 스크립트

---
+++ CameraRatio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRatio : MonoBehaviour
+{
+    public int width = 16;
+    public int height = 9;
+
+    private void Awake()
+    {
+        Camera camera = GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float ScaleWidth;
+        float ScaleHeight;
+
+        ScaleHeight = ((float)Screen.width / Screen.height) / ((float)width / height);
+        ScaleWidth = 1.0f / ScaleHeight;
+        if (ScaleHeight < 1)
+        {
+            rect.height = ScaleHeight;
+            rect.y = (1.0f - ScaleHeight) / 2.0f;
+        }
+        else
+        {
+            rect.width = ScaleWidth;
+            rect.x = (1.0f - ScaleWidth) / 2.0f;
+        }
+        camera.rect = rect;
+    }
+}
